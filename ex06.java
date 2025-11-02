8faf731a36f4ea3ae96f09d6c3f3fc09bfcddc6f
beatriz <108158611+bcanutoo@users.noreply.github.com>
2025-11-02 12:50:09 -0300

    ex6

---
+++ ex06.java
@@ -0,0 +1,14 @@
+public class Exercicio06 {
+    public static void main(String[] args) {
+        int[] A = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] B = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        int[] C = new int[A.length];
+
+        for (int i = 0; i < A.length; i++) {
+            C[i] = A[i] * B[i];
+        }
+
+        System.out.print("Vetor C (multiplicação): ");
+        for (int valor : C) System.out.print(valor + " ");
+    }
+}
